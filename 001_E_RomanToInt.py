3edd866bab2245dd2222c9959311ac0a7cc3f8eb
Ashutosh Srivastava <erashu@live.in>
2025-09-25 21:45:33 +0530

    test

--- 001_E_RomanToInt.py
+++ 001_E_RomanToInt.py
@@ -1,7 +1,12 @@
 def romanToInt(s: str) -> int:
     roman_map = {
-        'I': 1, 'V': 5, 'X': 10, 'L': 50,
-        'C': 100, 'D': 500, 'M': 1000
+        'I': 1, 
+        'V': 5, 
+        'X': 10, 
+        'L': 50,
+        'C': 100, 
+        'D': 500, 
+        'M': 1000
     }
     total_value = 0
     prev_value = 0
